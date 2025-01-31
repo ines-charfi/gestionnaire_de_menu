bc064ca2ad6a47b14ca6190c6770c53b01891e3e
Alex.Bachir <alex.bachir@laplateforme.io>
2025-01-31 15:07:40 +0100

    connexion envoyée

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+<header>
+<nav>
+        <ul>
+            <li><a href="index.php">Registration</a></li>
+            <li><a href="login.php">Login</a></li>
+            
+        </ul>
+</nav>
+<h1>Bienvenu ! </h1>
+</header>
+</body>
+</html>
